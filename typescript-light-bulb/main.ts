776b9c429a46d2d9da09b6cc6e60236619374d28
Bryant Mendoza <Bryantthbest@gmail.com>
2024-07-23 23:41:38 +0000

    completed typescript-light-bulb exercise

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,19 @@
+let lightSwitch = false;
+
+const $bulb = document.querySelector('.bulb');
+const $bulbBackground = document.querySelector('.bulb-background');
+if (!$bulb) throw new Error('$bulb query has failed');
+$bulb.addEventListener('click', clickHandler);
+function clickHandler(): void {
+  if (!$bulbBackground) throw new Error('$bulbBackground query has failed');
+  if (!$bulb) throw new Error('$bulb query has failed');
+  if (lightSwitch === false) {
+    lightSwitch = true;
+    $bulb.className = 'bulb bulb-on';
+    $bulbBackground.className = 'bulb-background background-on';
+  } else {
+    lightSwitch = false;
+    $bulb.className = 'bulb bulb-off';
+    $bulbBackground.className = 'bulb-background background-off';
+  }
+}
