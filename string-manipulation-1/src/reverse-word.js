ab86493f7f894d738123b9cca75832da7ffa62d8
Bxvant <95056102+Bxvant@users.noreply.github.com>
2024-08-05 16:10:33 -0700

    Delete string-manipulation-1/src/reverse-word.js

--- string-manipulation-1/src/reverse-word.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* exported reverseWord */
-function reverseWord(word) {
-  let reversedChar = '';
-  for (let i = word.length - 1; i >= 0; i--) {
-    reversedChar += word.charAt(i);
-  }
-  return reversedChar;
-}
