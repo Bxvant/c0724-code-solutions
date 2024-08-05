f5e86057472ca75f9abccdc527abba9e6264fa52
Bxvant <95056102+Bxvant@users.noreply.github.com>
2024-08-05 16:29:05 -0700

    Updated compact.ts

--- array-manipulation-1/src/compact.ts
+++ array-manipulation-1/src/compact.ts
@@ -1,17 +1,9 @@
 /* exported compact */
-function compact(array: any[]): any[] {
-  const goodValues: any[] = [];
+function compact(array: unknown[]): unknown[] {
+  const goodValues = [];
   for (let i = 0; i < array.length; i++) {
-    const value = array[i];
-    if (
-      value !== false &&
-      value !== null &&
-      value !== undefined &&
-      value !== '' &&
-      !Number.isNaN(value) &&
-      value !== 0
-    ) {
-      goodValues.push(value);
+    if (array[i]) {
+      goodValues.push(array[i]);
     }
   }
   return goodValues;
