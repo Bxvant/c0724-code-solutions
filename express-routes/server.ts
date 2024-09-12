740bdafaf62ce9ebdb2262582ba939bd04d4ea9a
Bryant Mendoza <Bryantthbest@gmail.com>
2024-09-12 17:48:59 +0000

    updated calls location

--- express-routes/server.ts
+++ express-routes/server.ts
@@ -2,20 +2,20 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('test number one, slash');
+});
+
+app.get('/notes', (req, res) => {
+  res.send('test number two , notes');
+});
+
+app.post('/notes/123', (req, res) => {
+  res.send('test number three, 123');
+});
+
 app.use((req, res, next) => {
   console.log('The date is', new Date());
-
-  app.get('/', (req, res) => {
-    res.send('test number one, slash');
-  });
-
-  app.get('/notes', (req, res) => {
-    res.send('test number two , notes');
-  });
-
-  app.post('/notes/123', (req, res) => {
-    res.send('test number three, 123');
-  });
 
   next();
 });
